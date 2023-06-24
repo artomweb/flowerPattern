639be96ac20017a10e57e72366e9fac985548e1e
artom web <artomweb@gmail.com>
2023-06-24 17:27:03 +0100

    working bezier

--- flower.js
+++ flower.js
@@ -1,4 +1,4 @@
-const colours = ["#d95a8d", "#f6f398", "#AED6D5", "#7eca72", "#efa85e"];
+const colours = ["#d95a8d", "#f6f398", "#AED6D5", "#7eca72", "#efa85e", "#efa85e", "#efa85e"];
 
 class Flower {
   constructor(x, y, r, i) {
@@ -34,45 +34,77 @@ class Flower {
 
     beginShape();
 
+    // circle(0, 0, 50);
+
     for (let i = 0; i < this.numVertices + 1; i++) {
+      // fill(colours[i]);
       let thisRns = this.rndNmbs[i];
       const angle = i * spacing;
-      let angler = angle + thisRns * 10;
-      const x = cos(radians(angler)) * this.r;
-      const y = sin(radians(angler)) * this.r;
+      const y = sin(radians(angle)) * this.r;
+      const x = cos(radians(angle)) * this.r;
 
       if (i == 0) {
         vertex(x, y);
       } else {
-        const cAngle = angler - spacing / 2;
-        const cX = cos(radians(cAngle)) * (this.r * (1.5 + thisRns));
-        const cY = sin(radians(cAngle)) * (this.r * (1.5 + thisRns));
-        quadraticVertex(cX, cY, x, y);
+        const cAngle = (i - 1) * spacing + spacing / 4;
+        const cX = cos(radians(cAngle)) * this.r * 1.5;
+        const cY = sin(radians(cAngle)) * this.r * 1.5;
+        const cAngle2 = i * spacing - spacing / 4;
+        const cX2 = cos(radians(cAngle2)) * this.r * 1.5;
+        const cY2 = sin(radians(cAngle2)) * this.r * 1.5;
+
+        const angle2 = i * spacing;
+        const aX = cos(radians(angle2)) * this.r;
+        const aY = sin(radians(angle2)) * this.r;
+
+        bezierVertex(cX, cY, cX2, cY2, aX, aY);
       }
     }
     endShape();
 
-    fill(this.insideCol);
+    // noFill();
 
     beginShape();
+
+    fill(this.insideCol);
+
+    // circle(0, 0, 50);
 
     const insideRadius = this.r - this.r / 5;
 
     for (let i = 0; i < this.numVertices + 1; i++) {
+      // fill(colours[i]);
       let thisRns = this.rndNmbs[i];
       const angle = i * spacing;
-      let angler = angle + thisRns * 10;
+      let angler = angle;
       const x = cos(radians(angler)) * insideRadius;
       const y = sin(radians(angler)) * insideRadius;
 
       if (i == 0) {
         vertex(x, y);
       } else {
-        const cAngle = angler - spacing / 2;
-        const cX = cos(radians(cAngle)) * (insideRadius * (1.5 + thisRns));
-        const cY = sin(radians(cAngle)) * (insideRadius * (1.5 + thisRns));
-        quadraticVertex(cX, cY, x, y);
+        const cAngle = (i - 1) * spacing + spacing / 4;
+        const cX = cos(radians(cAngle)) * insideRadius * 1.5;
+        const cY = sin(radians(cAngle)) * insideRadius * 1.5;
+        const cAngle2 = i * spacing - spacing / 4;
+        const cX2 = cos(radians(cAngle2)) * insideRadius * 1.5;
+        const cY2 = sin(radians(cAngle2)) * insideRadius * 1.5;
+
+        const angle2 = i * spacing;
+        const aX = cos(radians(angle2)) * insideRadius;
+        const aY = sin(radians(angle2)) * insideRadius;
+        // quadraticVertex(cX, cY, x, y);
+        // if (i == 1) {
+        // console.log(x, y, cX, cY, cX2, cY2);
+        bezierVertex(cX, cY, cX2, cY2, aX, aY);
+        // vertex(x, y);
+        // push();
+        // fill((i / this.numVertices) * 220);
+        // circle(x, y, 20);
         // circle(cX, cY, 20);
+        // circle(cX2, cY2, 20);
+        // pop();
+        // }
       }
     }
     endShape();
--- index.js
+++ index.js
@@ -12,53 +12,65 @@ function setup() {
   //   strokeWeight(5);
   noStroke();
 
-  for (let c = 0; c < 10000; c++) {
-    let f = new Flower(random(width), random(height), 50, flowers.length);
-    let overlap = false;
-    for (var j = 0; j < flowers.length; j++) {
-      var other = flowers[j];
-      var d = dist(f.pos.x, f.pos.y, other.pos.x, other.pos.y);
-      if (d < f.r * 1.7 + other.r * 1.7) {
-        overlap = true;
-      }
-    }
-    if (!overlap) {
-      flowers.push(f);
-    }
-    // if (flowers.length > numFlowers - 1) break;
-  }
-  for (let c = 0; c < 10000; c++) {
-    let f = new Flower(random(width), random(height), 35, flowers.length);
-    let overlap = false;
-    for (var j = 0; j < flowers.length; j++) {
-      var other = flowers[j];
-      var d = dist(f.pos.x, f.pos.y, other.pos.x, other.pos.y);
-      if (d < f.r * 1.7 + other.r * 1.7) {
-        overlap = true;
-      }
-    }
-    if (!overlap) {
-      flowers.push(f);
-    }
-    // if (flowers.length > numFlowers - 1) break;
-  }
+  // for (let c = 0; c < 10000; c++) {
+  //   let f = new Flower(random(width), random(height), 100, flowers.length);
+  //   let overlap = false;
+  //   for (var j = 0; j < flowers.length; j++) {
+  //     var other = flowers[j];
+  //     var d = dist(f.pos.x, f.pos.y, other.pos.x, other.pos.y);
+  //     if (d < f.r * 1.7 + other.r * 1.7) {
+  //       overlap = true;
+  //     }
+  //   }
+  //   if (!overlap) {
+  //     flowers.push(f);
+  //   }
+  //   // if (flowers.length > numFlowers - 1) break;
+  // }
+  // for (let c = 0; c < 10000; c++) {
+  //   let f = new Flower(random(width), random(height), 35, flowers.length);
+  //   let overlap = false;
+  //   for (var j = 0; j < flowers.length; j++) {
+  //     var other = flowers[j];
+  //     var d = dist(f.pos.x, f.pos.y, other.pos.x, other.pos.y);
+  //     if (d < f.r * 1.7 + other.r * 1.7) {
+  //       overlap = true;
+  //     }
+  //   }
+  //   if (!overlap) {
+  //     flowers.push(f);
+  //   }
+  //   // if (flowers.length > numFlowers - 1) break;
+  // }
 
-  flower = new Flower(width / 2, height / 2, 100);
+  flower = new Flower(width / 2, height / 2, 200);
 }
 
 function draw() {
   background("white");
   translate(-width / 2, -height / 2);
-  //   flower.paint();
-  fill("red");
-  for (let i = 0; i < flowers.length; i++) {
-    flowers[i].paint();
-    // circle(flowers[i].pos.x, flowers[i].pos.y, flowers[i].r * 1.7 * 2);
-  }
+  flower.paint();
+  // fill("red");
+  // for (let i = 0; i < flowers.length; i++) {
+  //   flowers[i].paint();
+  //   // circle(flowers[i].pos.x, flowers[i].pos.y, flowers[i].r * 1.7 * 2);
+  // }
+
+  translate(flower.pos.x, flower.pos.y);
+
+  // noFill();
+  // strokeWeight(10);
+  // stroke("pink");
+  // beginShape();
+  // vertex(49.44271909999159, 152.16904260722455);
+  // // circle(25, 30, 20);
+  // bezierVertex(-141.06846055019352, 194.1640786499874, -228.25356391083682, 74.1640786499874, -129.44271909999162, -94.04564036679568);
+  // // circle(25, 30, 20);
+  // endShape();
   // texture(img);
   // fill(0, 0, 0, 0);
-  tint(255, 100);
-  image(img, 0, 0, width, height);
+  // tint(255, 100);
+  // image(img, 0, 0, width, height);
   //   circle(width / 2, height / 2, 250);
   noLoop();
 }
